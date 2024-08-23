0eef6e093c96ccf1066c4bdb9c76b9c77406af3a
Arttacker <salehadel2001@gmail.com>
2024-08-23 05:02:43 +0300

    added session-4-vulnerable-code

---
+++ app.py
@@ -0,0 +1,108 @@
+from flask import Flask, render_template, request, redirect, url_for, session, flash, Markup, escape
+import db
+import utils
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
+app = Flask(__name__)
+connection = db.connect_to_database()
+app.secret_key = "SUPER-SECRET"
+limiter = Limiter(app=app, key_func=get_remote_address, default_limits=["50 per minute"], storage_uri="memory://")
+app.config['SESSION_COOKIE_HTTPONLY'] = False
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        if session['username'] == 'admin':
+            return list(db.get_all_users(connection))
+        else:
+            return f"Welcome, {session['username']}!"
+    return "You are not logged in."
+
+@app.route('/login', methods=['GET', 'POST'])
+@limiter.limit("10 per minute")
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = db.get_user(connection, username)
+
+        if user:
+            if utils.is_password_match(password, user[2]):
+                session['username'] = user[1]
+                session['user_id'] = user[0]
+                return redirect(url_for('index'))
+            else:
+                flash("Invalid username or password", "danger")
+                return render_template('login.html')
+
+        else:
+            flash("Invalid username or password", "danger")
+            return render_template('login.html')
+
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+@limiter.limit("10 per minute")
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if not utils.is_strong_password(password):
+            flash("Sorry You Entered a weak Password Please Choose a stronger one", "danger")
+            return render_template('register.html')
+
+        user = db.get_user(connection, username)
+        if user:
+            flash("Username already exists. Please choose a different username.", "danger")
+            return render_template('register.html')
+        else:
+            db.add_user(connection, username, password)
+            return redirect(url_for('login'))
+
+    return render_template('register.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+#--------------------------------------------------------------Session 4-------------------------------------------------------------#
+@app.route('/CheckStock')
+def Check_Stock():
+    server = request.args.get("server")
+    return redirect(server)
+#------------------------------------------------------------------------------------------------------------------------------------#
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    search_query = Markup(request.args.get('search_query'))
+    usernames = db.search_users(connection, search_query)
+    return render_template('search_results.html', usernames=usernames, search_query=search_query)
+
+
+@app.route('/comments', methods=['GET', 'POST'])
+def addComment():
+    comments = db.get_comments(connection)
+
+    if request.method == 'POST':
+        text = Markup(request.form['comment'])
+        username = session.get('username')
+        if username:
+            db.add_comment(connection, username, text)
+            comments = db.get_comments(connection)
+        else:
+            flash("You must be logged in to post a comment.", "warning")
+
+    return render_template('comments.html', comments=comments)
+
+@app.route('/clear_comments', methods=['GET','POST'])
+def clearComments():
+    db.clear_comments(connection)
+    return redirect(url_for('addComment'))
+#--------------------------------------------------------------------------------------------------------------------------------
+if __name__ == '__main__':
+    db.init_db(connection)
+    db.init_comments_table(connection)
+    db.seed_admin_user(connection)
+    app.run(debug=True)
---
+++ db.py
@@ -0,0 +1,96 @@
+import utils
+
+
+def connect_to_database(name='database.db'):
+    import sqlite3
+    return sqlite3.connect(name, check_same_thread=False)
+
+
+def init_db(connection):
+    cursor = connection.cursor()
+
+    cursor.execute('''
+		CREATE TABLE IF NOT EXISTS users (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			username TEXT NOT NULL UNIQUE,
+			password TEXT NOT NULL
+		)
+	''')
+
+    connection.commit()
+
+
+def add_user(connection, username, password):
+    cursor = connection.cursor()
+    hashed_password = utils.hash_password(password)
+    query = '''INSERT INTO users (username, password) VALUES (?, ?)'''
+    cursor.execute(query, (username, hashed_password))
+    connection.commit()
+
+
+def get_user(connection, username):
+    cursor = connection.cursor()
+    query = '''SELECT * FROM users WHERE username = ?'''
+    cursor.execute(query, (username,))
+    return cursor.fetchone()
+
+
+def get_all_users(connection):
+    cursor = connection.cursor()
+    query = 'SELECT * FROM users'
+    cursor.execute(query)
+    return cursor.fetchall()
+
+
+def seed_admin_user(connection):
+    admin_username = 'admin'
+    admin_password = 'admin'
+
+    # Check if admin user exists
+    admin_user = get_user(connection, admin_username)
+    if not admin_user:
+        add_user(connection, admin_username, admin_password)
+        print("Admin user seeded successfully.")
+
+
+def search_users(connection, search_query):
+    cursor = connection.cursor()
+    query = '''SELECT username FROM users WHERE username LIKE ?'''
+    cursor.execute(query, (f"%{search_query}%",))
+    return cursor.fetchall()
+
+
+def init_comments_table(connection):
+    cursor = connection.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS comments (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL,
+            text TEXT NOT NULL,
+            timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+    ''')
+
+    connection.commit()
+
+def add_comment(connection, username, text):
+    cursor = connection.cursor()
+    query = '''INSERT INTO comments (username, text) VALUES (?, ?)'''
+    cursor.execute(query, (username, text))
+    connection.commit()
+
+def get_comments(connection):
+    cursor = connection.cursor()
+    query = '''
+        SELECT comments.username, comments.text, comments.timestamp
+        FROM comments
+    '''
+    cursor.execute(query)
+    return cursor.fetchall()
+
+def clear_comments(connection):
+    cursor = connection.cursor()
+    query = '''DELETE FROM comments'''
+    cursor.execute(query)
+    connection.commit()
